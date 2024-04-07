a766803aaf433722d32035f2b1cc37e7ab154107
rayhan-cs <rayhanhasanur@gmail.com>
2024-04-07 15:49:58 +0600

    solved

---
+++ BeeCrowd/1016 - Distance.cs
@@ -0,0 +1,17 @@
+using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        var Y = input * 2;
+
+        Console.WriteLine(Y + " minutos");
+
+    }
+
+}
