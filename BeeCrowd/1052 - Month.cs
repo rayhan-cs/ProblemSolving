26f5080748f3c5c6f0a7dc6da344699683de2722
rayhan-cs <rayhanhasanur@gmail.com>
2024-04-21 17:16:57 +0600

    1052 - Month Done

---
+++ BeeCrowd/1052 - Month.cs
@@ -0,0 +1,62 @@
+using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        var number = Convert.ToInt32(Console.ReadLine());
+
+        if (number == 1)
+        {
+            Console.WriteLine("January");
+        }
+        if (number == 2)
+        {
+            Console.WriteLine("February");
+        }
+        if (number == 3)
+        {
+            Console.WriteLine("March");
+        }
+        if (number == 4)
+        {
+            Console.WriteLine("April");
+        }
+        if (number == 5)
+        {
+            Console.WriteLine("May");
+        }
+        if (number == 6)
+        {
+            Console.WriteLine("June");
+        }
+        if (number == 7)
+        {
+            Console.WriteLine("July");
+        }
+        if (number == 8)
+        {
+            Console.WriteLine("August");
+        }
+        if (number == 9)
+        {
+            Console.WriteLine("September");
+        }
+        if (number == 10)
+        {
+            Console.WriteLine("October");
+        }
+        if (number == 11)
+        {
+            Console.WriteLine("November");
+        }
+        if (number == 12)
+        {
+            Console.WriteLine("December");
+        }
+
+    }
+
+}
